701c2ccde92b0857571c3d76ae16a69013a81a86
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2020-04-17 01:36:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join()
+  when 2
+    array.join(" and ")
+  else
+    word = array.pop
+    array.join(", ") << ", and #{word}"
+  end
 end
