14eed840e86a5b1136b64cf31874545648fc8658
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 17:59:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+#array = ["fiddleheads","okra","kohlrabi"]
+#array = ["fiddleheads","okra"]
 
+def oxford_comma(array)
+ final_item = array[-1]
+     array.pop
+ 
+ if array.size > 1
+
+    array.join(", ")
+    complete_item = array.join(", ") + ", and #{final_item}"
+    return complete_item
+  elsif array.size == 1
+  first_item = array[0]
+    complete_item = "#{first_item}" + " and #{final_item}"
+    return complete_item
+  else
+    return final_item
+  end
+#  puts array
+#  puts complete_item
+#  puts reduced_array
 end
+
+#oxford_comma(array)
