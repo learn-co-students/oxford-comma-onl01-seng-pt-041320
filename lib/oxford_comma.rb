7bbcea64d00050ed6df51799eb6963ffb279ed70
Joey <joey.alzamora@gmail.com>
2020-04-23 20:31:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,9 @@ if array.size == 1
   array.join
 elsif array == 2 
   array.join(" and ")
-else
-  array[0..-2].join(", ") + ", and " + array[-1]
+else 
+  array[0..-2].join(", ") + " and " + array[-1] 
   end
- 
-
 end
 
 
