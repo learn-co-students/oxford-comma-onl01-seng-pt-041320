ab0d52c6f6ebf6e423c52d0fe52c115e0c5237de
Jared  E <jared.epps322@gmail.com>
2020-04-24 03:39:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,51 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  
+  if array.length == 1
+    array[0]
+  elsif array.length == 2 
+    array[1] 
+    array.join(" and ")
+  else 
+    array[array.length-1 ] = "and " << array[array.length-1 ]
+    array.join(", ") 
+  end
 end
+
+
+
+
+
+
+
+
+# def oxford_comma(array)
+#   if array.length==1 
+#     puts array[0]
+#   else 
+#     last=array[array.length-1]
+#     array.pop
+#     string=array.join(', ') 
+#     if array.length==length-1
+#       string.concat(" and ")
+#     else 
+#       string.concat(", and ")
+#     end 
+#     string.concat(last)
+#     puts string
+#   end 
+# end 
+    
+
+# def oxford_comma(array)
+  
+#   if array.length == 1
+#     array[0]
+#   elsif array.length == 2 
+#     array[1]  + "and"
+#   else 
+#     array[array.length-1 ] = "and " << array[array.length-1 ]
+#     array.join(", ") 
+#   end
+# end
+
