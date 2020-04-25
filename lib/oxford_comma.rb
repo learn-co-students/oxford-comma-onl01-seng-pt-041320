9d149f8ac1bda8427f5ef830cded75ab78779b61
Sara Amiry <saraamiry24@gmail.com>
2020-04-25 02:53:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  ["kiwi"]
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    #array.join(" and ")
+   # array.insert(1, "and").join(" ")
+   "#{array[0]} and #{array[1]}"
+  else
+   # "#{array[0,,-2].join(", ")}, and #{array[-1]}"
+   array[-1].insert(0, "and ")
+   array.join(", ")
+  end
 end
