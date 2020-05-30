e06392d5b744536960c06853f10474b80e0741b8
chica25 <patricia@patriciaco.com>
2020-05-30 16:13:10 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -21,16 +21,37 @@ require "pry"
 # end
 
 
+#   def oxford_comma(array)
+#     if array.size == 1 
+#       array.join
+#     elsif array.size == 2
+#       array.join(" and ")
+#     elsif array.size >= 3
+#       array[-1] = "and " << array[-1]
+#       array.join(", ")
+#     else 
+#       array.join
+#   end
+# end
 
-  def oxford_comma(array)
-    if array.size == 1 
+#instructions
+#using the split method that creates a string in a array
+
+
+def oxford_comma(array)
+    #first failure -- 1-element array["Kiwi"]
+    if array.length == 1
       array.join
-    elsif array.size == 2
-      array.join(" and ")
-    elsif array.size >= 3
-      array[-1] = "and " << array[-1]
-      array.join(", ")
-    else 
-      array.join
-  end
+    # second failure - expect ["kiwi", "durian"] to eq["Kiwi and durian"]
+    elsif array.length == 2
+      #array.join(" and ")
+       #array.insert(1, "and ").join(" ")
+      "#{array[0]} and #{array[1]}"
+   # third failer "Kiwi, durian, and starfruit"
+    else
+      #array.length == 3
+    #   "#{array[0..-2].join(", ")}, and #{array[-1]}"
+        array[-1].insert(0, "and ")
+        array.join(", ")
+   end
 end
